e9d1b205a6db317ab0688d94f2ea06f5452b82b8
Aakash Sardana <aakashsardana3@gmail.com>
2017-08-14 13:29:51 +0530

    Add event listener to the calculate button

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //Hide the tip amount on load
 document.getElementById('totalTip').style.display = 'none'
+
+//Add Event listener to the button
+document.getElementById('calculate').addEventListener("click", function () {
+	calculateTip();
+});
