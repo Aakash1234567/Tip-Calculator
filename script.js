bd5f2a618eeaab7f886d6485778272c1153d3e1b
Aakash Sardana <aakashsardana3@gmail.com>
2017-08-14 14:28:46 +0530

    Add logic to calculate tip and finalize

--- script.js
+++ script.js
@@ -18,14 +18,31 @@ function calculateTip() {
 	if (billAmount === "" || serviceQuality == 0) {
 		window.alert("Please enter some values to get this baby up and running!!");
 		return;
+	}
+	//Check if numPeople input is empty or less than or equal to one
+	if ( numPeople == "" || numPeople <= 1 ) {
+		numPeople = 1;
+		document.getElementById("each").style.display = 'none';
+
+	} else {
+		document.getElementById("each").style.display = 'block';
+	}
+
+	var total = (billAmount * serviceQuality) / numPeople;
+	total = Math.round(total * 100) / 100;
+	total.toFixed(2);
+
+	document.getElementById("totalTip").style.display = "block";
+	document.getElementById("tip").innerHTML = total;
 
 }
 
 
 //Hide the tip amount on load
-document.getElementById('totalTip').style.display = 'none'
+document.getElementById('totalTip').style.display = 'none';
 
 //Add Event listener to the button
-document.getElementById('calculate').addEventListener("click", function () {
+var calculate = document.getElementById('calculate');
+calculate.addEventListener("click", function() {
 	calculateTip();
 });
